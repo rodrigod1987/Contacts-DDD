abbcfe3e0f1c167372bb5c149463aed88b64bcc7
Rodrigo Duarte <rodrigo@rodrigo.duarte>
2020-03-09 19:26:03 -0300

    Remoção de import não utilizado.

--- contacts-ddd-api/src/main/java/br/com/rodrigo/contacts/api/controller/PhoneController.java
+++ contacts-ddd-api/src/main/java/br/com/rodrigo/contacts/api/controller/PhoneController.java
@@ -2,8 +2,6 @@ package br.com.rodrigo.contacts.api.controller;
 
 import java.util.Collection;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
