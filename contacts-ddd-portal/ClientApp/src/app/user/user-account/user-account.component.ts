be6af504eea9aa02eef3439b05aafd6c0790a9e2
Rodrigo Duarte <rodrigod1987@gmail.com>
2020-04-22 11:15:34 -0300

    Creating user-account view

--- contacts-ddd-portal/ClientApp/src/app/user/user-account/user-account.component.ts
+++ contacts-ddd-portal/ClientApp/src/app/user/user-account/user-account.component.ts
@@ -1,13 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/shared/services/user.service';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/shared/model/user';
 
 @Component({
   templateUrl: './user-account.component.html'
 })
 export class UserAccountComponent implements OnInit{
 
-  constructor() { }
+  user$: Observable<User>;
+
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.user$ = this.userService.getUser();
+  }
+
+  edit() {
+
+  }
+
+  save() {
+
+  }
+
+  goBack() {
+
   }
 
 }
--- contacts-ddd-portal/ClientApp/src/app/user/user.module.ts
+++ contacts-ddd-portal/ClientApp/src/app/user/user.module.ts
@@ -6,6 +6,7 @@ import { RouterModule } from '@angular/router';
 import { UserAccountComponent } from './user-account/user-account.component';
 import { UserComponent } from './user.component';
 import { UserRoutingModule } from './user.routing.module';
+import { DirectivesModule } from '../shared/directives/directives.module';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { UserRoutingModule } from './user.routing.module';
     FormsModule,
     ReactiveFormsModule,
     RouterModule,
+    DirectivesModule,
     UserRoutingModule
   ],
 })
