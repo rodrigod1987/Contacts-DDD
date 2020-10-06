c62acc4a2b22a5d384044dca87907ec0fec2cfbf
Rodrigo Duarte <rodrigo.duarte@totvs.com.br>
2020-10-06 14:00:02 -0300

    Adjusting mail properties.

--- contacts-ddd-api/src/main/java/br/com/rodrigo/contacts/api/configuration/MailConfiguration.java
+++ contacts-ddd-api/src/main/java/br/com/rodrigo/contacts/api/configuration/MailConfiguration.java
@@ -2,42 +2,36 @@ package br.com.rodrigo.contacts.api.configuration;
 
 import java.util.Properties;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
 public class MailConfiguration {
-	
-	@Autowired
-	private Environment env;
 
 	@Bean
 	public JavaMailSender javaMailSender() {
 		
-		JavaMailSenderImpl mailSenderImpl = new JavaMailSenderImpl();
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+		mailSender.setHost("smtp.gmail.com");
+	    mailSender.setPort(587);
+	    mailSender.setUsername("contacts.ddd.suporte@gmail.com");
+	    mailSender.setPassword("qkvvqvcjdwaigykn");
+	    
+	    mailSender.setJavaMailProperties(this.getMailProperties(mailSender));
 		
-		mailSenderImpl.setHost(env.getProperty("mail.host"));
-		mailSenderImpl.setPort(Integer.parseInt(env.getProperty("mail.port")));
-		mailSenderImpl.setUsername(env.getProperty("mail.username"));
-		mailSenderImpl.setPassword(env.getProperty("mail.password"));
-		
-		mailSenderImpl.setJavaMailProperties(getMailProperties());
-		
-		return mailSenderImpl;
+		return mailSender;
 		
 	}
 	
-	private Properties getMailProperties() {
-        Properties properties = new Properties();
-        properties.setProperty("mail.transport.protocol", env.getProperty("mail.transport.protocol"));
-        properties.setProperty("mail.smtp.auth", env.getProperty("mail.smtp.auth"));
-        properties.setProperty("mail.smtp.starttls.enable", env.getProperty("mail.smtp.starttls.enable"));
-        properties.setProperty("mail.debug", env.getProperty("mail.debug"));
-        return properties;
+	private Properties getMailProperties(JavaMailSenderImpl mailSender) {
+		Properties props = mailSender.getJavaMailProperties();
+	    props.put("mail.transport.protocol", "smtp");
+	    props.put("mail.smtp.auth", "true");
+	    props.put("mail.smtp.starttls.enable", "true");
+	    props.put("mail.debug", "true");
+        return props;
     }
 	
 }
