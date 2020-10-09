c16bc1b47da4ab6eed6a4893b438b007ea1cb3c2
Rodrigo Duarte <rodrigo.duarte@totvs.com.br>
2020-10-09 19:43:22 -0300

    Adjusting migrations, removing from maven and run on app startup

---
+++ contacts-ddd-api/src/main/java/br/com/rodrigo/contacts/api/configuration/FlywayMigration.java
@@ -0,0 +1,31 @@
+package br.com.rodrigo.contacts.api.configuration;
+
+import javax.sql.DataSource;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FlywayMigration {
+	
+	@Autowired
+	private DataSource datasource;
+
+	@Bean
+	public FlywayMigrationStrategy flywayMigrationStrategy() {
+		
+		Flyway flyway = Flyway
+				.configure()
+				.dataSource(this.datasource)
+				.load();
+		
+		return flywayStrategy -> {
+			flyway.migrate();			
+		};
+	}
+	
+	
+}
